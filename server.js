f3695c81d8563c81113172929006bd597b1e45ce
RohitHadiya7 <rohithadiya777@gmail.com>
2025-04-04 18:08:59 +0530

    changed vercel json

--- server.js
+++ server.js
@@ -13,11 +13,7 @@ const port = process.env.PORT || 5000
 
 import cors from 'cors';
 
-app.use(cors({
-  origin: process.env.FRONTEND_URL,
-  methods: ['GET', 'POST', 'PUT', 'DELETE'],
-  credentials: true 
-}));
+app.use(cors())
 
 app.use(express.json())
 
