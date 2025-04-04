f16b73878fe2707652774a03af19e9662e2485d1
RohitHadiya7 <rohithadiya777@gmail.com>
2025-04-04 20:26:37 +0530

    added name in model and authcontroller

--- controllers/authController.js
+++ controllers/authController.js
@@ -4,8 +4,15 @@ import User from '../models/User.js'
 
 const registerUser = async (req, res) => {
   try {
-    const hashedPassword = await bcrypt.hash(req.body.password, 10)
-    const user = new User({ email: req.body.email, password: hashedPassword })
+    const { name, email, password } = req.body
+    const hashedPassword = await bcrypt.hash(password, 10)
+
+    const user = new User({
+      name: name,
+      email: email,
+      password: hashedPassword,
+    })
+
     await user.save()
     res.status(201).send('User registered successfully')
   } catch (error) {
@@ -22,8 +29,11 @@ const loginUser = async (req, res) => {
 
   try {
     if (await bcrypt.compare(req.body.password, user.password)) {
-      const accessToken = jwt.sign({ _id: user._id, email: user.email }, process.env.JWT_SECRET)
-      res.json({ accessToken: accessToken, userId: user._id })
+      const accessToken = jwt.sign(
+        { _id: user._id, email: user.email, name: user.name },
+        process.env.JWT_SECRET
+      )
+      res.json({ accessToken: accessToken, userId: user._id, name: user.name })
     } else {
       res.status(401).send('Incorrect password')
     }
